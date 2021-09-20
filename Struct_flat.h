698b4340b786ed3752e7da8d2fd94e067949d1de
PI03-Gavrilov <rol225rf.ru@mail.ru>
2021-09-20 10:55:36 +0700

    Add Struct Flat/.cpp/.h

---
+++ Struct_flat.h
@@ -0,0 +1,9 @@
+#pragma once
+/// Квартира
+struct {
+	int coin = 0; //цена
+	int countRoom = 0; //кол-во комнат
+	int numFlat = 0; //Номер квартиры
+} flat;
+
+inline void Flat(int coin, int countRoom, int numFlat);
---
+++ Stuct_flat.cpp
@@ -0,0 +1,7 @@
+#include "Struct_flat.h"
+
+inline void Flat(int coin, int countRoom, int numFlat) {
+	flat.coin = coin;
+	flat.countRoom = countRoom;
+	flat.numFlat = numFlat;
+}
